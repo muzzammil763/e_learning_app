b9f4fb826c6542c72dbd7f725c50557d36825a67
muzzammil763 <deadbase763@gmail.com>
2024-02-07 11:54:22 +0500

    added screen selecter page for easy navigating ...

---
+++ lib/Screens/screen_selecter.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ScreenSelecter extends StatelessWidget {
+  const ScreenSelecter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent[700],
+        title: const Text(
+          'Screen Selecter',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
